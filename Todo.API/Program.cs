466abde6a4983b6e2dd3df3a96d986ccd1ccfe30
bloma <blomaphiwe@gmail.com>
2025-11-18 09:53:06 +0200

    updating connection string

--- Todo.API/Program.cs
+++ Todo.API/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Todo.Application.Services;
 using Todo.Application.Services.Interface;
+using Todo.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ITodoItemService, TodoItemService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer("DefaultConnection");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
